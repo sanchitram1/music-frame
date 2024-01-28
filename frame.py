b8f6c869d5e9db8fef27bc8ec83c5cf729998bac
Sanchit Ram Arvind <sanchitram@gmail.com>
2024-01-28 01:25:27 -0700

    x

--- frame.py
+++ frame.py
@@ -1,12 +1,10 @@
 from flask import Flask
-import json
 
 app = Flask(__name__)
 
-with open("data.json", "r") as f:
-    data = json.load(f)
-
-print(data)
+image = "https://i.scdn.co/image/ab67616d0000b27350068ff4b9b746b7cf2388aa"
+song = "Idol Eyes"
+artist = "Common Saints"
 
 
 @app.route("/")
@@ -18,34 +16,31 @@ def index():
           <meta charset="utf-8"/>
           <meta name="viewport" content="width=device-width"/>
           <meta property="og:title" content="Listen" />
+          <meta property="og:image" content="{image}" />
           <meta property="fc:frame" content="vNext" />
-          <meta property="fc:frame:image" content="{data['image']}" />
+          <meta property="fc:frame:image" content="{image}" />
           <meta property="fc:frame:button:1" content="Listen" />
-          <meta property="fc:frame:post_url" content="/listen" />
+          <meta property="fc:frame:post_url" content="https://frame-test-b1fbe9a5b014.herokuapp.com/listen" />
       </head>
     </html>
     """
 
 
-@app.route("/listen", methods=["POST", "GET"])
+@app.route("/listen", methods=["POST"])
 def listen():
     return f"""
     <!DOCTYPE html>
     <html>
       <head>
-          <meta property="og:title" content="{data['song']}" />
-          <meta property='og:image' content={data['image']} />
+          <meta property="og:title" content="{song}" />
+          <meta property='og:image' content={image} />
           <meta property="fc:frame" content="vNext" />
-          <meta property="fc:frame:image" content="{data['image']}" />
+          <meta property="fc:frame:image" content="{image}" />
       </head>
       <body>
-        <audio class="audio-player" controls autoplay loop>
-            <source src="{data['preview']}" type="audio/mpeg">
-            Your browser does not support the audio element.
-        </audio>
         <div>
-            <p>Song: {data['song']}</p>
-            <p>Artist: {data['artist']}</p>
+            <p>Song: {song}</p>
+            <p>Artist: {artist}</p>
         </div>
       </body>
     </html>
