54881389f79780e62a25280eca928a03eda63e4c
Sanchit Ram Arvind <sanchitram@gmail.com>
2024-01-28 08:51:14 -0700

    x

--- frame.py
+++ frame.py
@@ -1,14 +1,46 @@
-from flask import Flask
+from flask import Flask, request, session
+import secrets
 
 app = Flask(__name__)
+app.secret_key = secrets.token_urlsafe(16)
 
-image = "https://i.scdn.co/image/ab67616d0000b27350068ff4b9b746b7cf2388aa"
+preimage = "https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_Black.png"
+album = "https://i.scdn.co/image/ab67616d0000b27350068ff4b9b746b7cf2388aa"
 song = "Idol Eyes"
 artist = "Common Saints"
+preview = "https://p.scdn.co/mp3-preview/f5c6f9354fa9902d2ea5406c64b2a53ec70755e3?cid=467193f35468464cbda5989d0ebe23e7"
+
+# <meta property="fc:frame:post_url" content="https://frame-test-5dd2ced0e874.herokuapp.com/listen" />
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        session["button_clicked"] = True
+        return render_page(True)
+    else:
+        return render_page(session.get("button_clicked", False))
+
+
+def render_page(button_clicked):
+    print(button_clicked)
+    image = preimage if not button_clicked else album
+    print(image)
+    button_html = (
+        """
+        <form method="post" action="/">
+            <button type="submit" name="button" value="Listen">Listen</button>
+        </form>
+    """
+        if not button_clicked
+        else ""
+    )
+    fc_frame_button = (
+        '<meta> property="fc:frame:button:1" content="Listen" />'
+        if not button_clicked
+        else ""
+    )
+
     return f"""
     <!DOCTYPE html>
     <html>
@@ -19,29 +51,11 @@ def index():
           <meta property="og:image" content="{image}" />
           <meta property="fc:frame" content="vNext" />
           <meta property="fc:frame:image" content="{image}" />
-          <meta property="fc:frame:button:1" content="Listen" />
-          <meta property="fc:frame:post_url" content="https://frame-test-b1fbe9a5b014.herokuapp.com/listen" />
-      </head>
-    </html>
-    """
-
-
-@app.route("/listen", methods=["POST"])
-def listen():
-    return f"""
-    <!DOCTYPE html>
-    <html>
-      <head>
-          <meta property="og:title" content="{song}" />
-          <meta property='og:image' content={image} />
-          <meta property="fc:frame" content="vNext" />
-          <meta property="fc:frame:image" content="{image}" />
+          {fc_frame_button}
+          <title> Listen </title>
       </head>
       <body>
-        <div>
-            <p>Song: {song}</p>
-            <p>Artist: {artist}</p>
-        </div>
+        {button_html}
       </body>
     </html>
     """
