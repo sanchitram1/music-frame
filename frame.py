af45444a2633d322481300843add3c43520181a1
Sanchit Ram Arvind <sanchitram@gmail.com>
2024-01-29 17:09:59 -0700

    x

--- frame.py
+++ frame.py
@@ -4,13 +4,10 @@ import secrets
 app = Flask(__name__)
 app.secret_key = secrets.token_urlsafe(16)
 
-preimage = "https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_Black.png"
 album = "https://i.scdn.co/image/ab67616d0000b27350068ff4b9b746b7cf2388aa"
 song = "Idol Eyes"
 artist = "Common Saints"
 preview = "https://p.scdn.co/mp3-preview/f5c6f9354fa9902d2ea5406c64b2a53ec70755e3?cid=467193f35468464cbda5989d0ebe23e7"
-
-# <meta property="fc:frame:post_url" content="https://frame-test-5dd2ced0e874.herokuapp.com/listen" />
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -24,8 +21,15 @@ def index():
 
 def render_page(button_clicked):
     print(button_clicked)
-    image = preimage if not button_clicked else album
+
+    # always display it as an image of the song you're listening to
+    image = album
     print(image)
+
+    # display the song and artist, only if the button has been clicked
+    tag_html = f'<meta property="og:description" content="{song} by {artist}" />'
+
+    # display the button only if it hasn't been clicked
     button_html = (
         """
         <form method="post" action="/">
@@ -37,7 +41,7 @@ def render_page(button_clicked):
     )
     print(button_html)
     fc_frame_button = (
-        '<meta> property="fc:frame:button:1" content="Listen" />'
+        '<meta property="fc:frame:button:1" content="Listen" />'
         if not button_clicked
         else ""
     )
@@ -51,14 +55,11 @@ def render_page(button_clicked):
           <meta name="viewport" content="width=device-width"/>
           <meta property="og:title" content="Listen" />
           <meta property="og:image" content="{image}" />
+          {tag_html}
           <meta property="fc:frame" content="vNext" />
           <meta property="fc:frame:image" content="{image}" />
           {fc_frame_button}
-          <title> Listen </title>
       </head>
-      <body>
-        {button_html}
-      </body>
     </html>
     """
 
